128c9c87bd51cc0826630c30e26452a5b6cb0376
Taylor Rees <hello@taylorrees.co>
2015-06-26 23:25:44 +0100

    Redraft of main website content.

---
+++ js/core.js
@@ -0,0 +1,173 @@
+// A function that defines the gallery carousel functions.
+// To invoke the function pass an array of images, the gallery
+// container and the preloader container.
+
+// The first child of the gallery must be 'controls'
+// The 'controls' child must then have 3 children:
+// 1. prev image
+// 2. next image
+// 3. image counter
+
+function carousel(images, gallery, preloader) {
+
+  var count = 0;
+  preloader.style.background = "url('" + images[count + 1] + "')";
+
+  // When the next image data action is called the
+  // next image will be loaded into placed and the
+  // (hidden) preloader will load the image next in sequence.
+
+  function nextImage(next) {
+    next.onclick = function() {
+      if (count != images.length - 1) {
+        count = count + 1;
+      } else {
+        count = 0;
+      }
+      gallery.style.background = "url('" + images[count] + "')";
+      if (count != images.length - 1) {
+        preloader.style.background = "url('" + images[count + 1] + "')";
+      } else {
+        preloader.style.background = "url('" + images[count - 1] + "')";
+      }
+      imageCount();
+    }
+  }
+
+  nextImage(gallery.children[0].children[1]);
+  nextImage(gallery.children[0].children[2]);
+
+  // When the previous image data action is called the
+  // previous image will be loaded into placed and the
+  // (hidden) preloader will load the previous image in sequence.
+
+  function prevImage(previous) {
+    previous.onclick = function() {
+      if (count != 0) {
+        count = count - 1;
+      } else {
+        count = images.length - 1;
+      }
+      gallery.style.background = "url('" + images[count] + "')";
+      if (count != 0) {
+        preloader.style.background = "url('" + images[count - 1] + "')";
+      } else {
+        preloader.style.background = "url('" + images[count + 1] + "')";
+      }
+      imageCount();
+    }
+  }
+
+  prevImage(gallery.children[0].children[0]);
+
+  // When the image in the gallery is changed the image
+  // counter will be appropriately updated.
+  function imageCount() {
+    var counter = gallery.children[0].children[2];
+    counter.innerHTML = (count + 1) + '/' + images.length;
+  }
+  imageCount();
+}
+
+// Give the before indoor gallery function
+var beforeIndoorGallery = document.getElementById('before-indoor-gallery');
+var beforeIndoorPreloader = document.getElementById('before-indoor-preloader');
+var beforeIndoorImages = [
+  'images/gallery/indoors/dafen-park-indoors-02.jpg',
+  'images/gallery/indoors/dafen-park-indoors-01.jpg',
+  'images/gallery/indoors/dafen-park-indoors-03.jpg',
+  'images/gallery/indoors/dafen-park-indoors-04.jpg',
+  'images/gallery/indoors/dafen-park-indoors-05.jpg',
+  'images/gallery/indoors/dafen-park-indoors-06.jpg',
+  'images/gallery/indoors/dafen-park-indoors-07.jpg',
+  'images/gallery/indoors/dafen-park-indoors-08.jpg',
+  'images/gallery/indoors/dafen-park-indoors-09.jpg',
+  'images/gallery/indoors/dafen-park-indoors-10.jpg',
+  'images/gallery/indoors/dafen-park-indoors-11.jpg',
+  'images/gallery/indoors/dafen-park-indoors-13.jpg',
+  'images/gallery/indoors/dafen-park-indoors-14.jpg',]
+
+carousel(beforeIndoorImages, beforeIndoorGallery, beforeIndoorPreloader);
+
+// Give the before outdoor gallery function
+var beforeOutdoorGallery = document.getElementById('before-outdoor-gallery');
+var beforeOutdoorPreloader = document.getElementById('before-outdoor-preloader');
+var beforeOutdoorImages = [
+  "images/gallery/outdoors/dafen-park-outdoors-11.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-08.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-01.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-02.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-03.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-04.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-05.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-06.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-10.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-12.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-13.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-14.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-15.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-16.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-17.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-18.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-19.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-20.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-21.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-22.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-23.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-24.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-25.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-26.jpg"]
+
+carousel(beforeOutdoorImages, beforeOutdoorGallery, beforeOutdoorPreloader);
+
+// Images of the finished building are still to be added.
+
+/*
+
+// Give the before indoor gallery function
+var afterIndoorGallery = document.getElementById('after-indoor-gallery');
+var afterIndoorPreloader = document.getElementById('after-indoor-preloader');
+var afterIndoorImages = [
+  "images/gallery/indoors/dafen-park-indoors-14.jpg",
+  "images/gallery/indoors/dafen-park-indoors-13.jpg",
+  "images/gallery/indoors/dafen-park-indoors-11.jpg",
+  "images/gallery/indoors/dafen-park-indoors-10.jpg",
+  "images/gallery/indoors/dafen-park-indoors-09.jpg",
+  "images/gallery/indoors/dafen-park-indoors-08.jpg",
+  "images/gallery/indoors/dafen-park-indoors-07.jpg",
+  "images/gallery/indoors/dafen-park-indoors-06.jpg"]
+
+carousel(afterIndoorImages, afterIndoorGallery, afterIndoorPreloader);
+
+// Give the before outdoor gallery function
+var afterOutdoorGallery = document.getElementById('after-outdoor-gallery');
+var afterOurdoorPreloader = document.getElementById('after-outdoor-preloader');
+var afterOutdoorImages = [
+  "images/gallery/outdoors/dafen-park-outdoors-11.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-08.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-01.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-02.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-03.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-04.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-05.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-06.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-10.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-12.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-13.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-14.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-15.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-16.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-17.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-18.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-19.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-20.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-21.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-22.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-23.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-24.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-25.jpg",
+  "images/gallery/outdoors/dafen-park-outdoors-26.jpg"]
+
+carousel(afterOutdoorImages, afterOutdoorGallery, afterOurdoorPreloader);
+
+*/
